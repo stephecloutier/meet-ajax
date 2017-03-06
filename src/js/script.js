e3505674c020eb998095391a1df1e3ae69f874f7
stephecloutier <stephecloutier@gmail.com>
2017-03-06 14:19:08 +0100

    Checked for errors and displayed them if any

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,8 @@
  * started at 20/02/2017
  */
 
-let $form;
+let $form,
+    $tplErrorMessage;
 
 const fHandleSubmit = function(oEvent) {
     let $nameInput = $form.find("input[name='name']"),
@@ -16,12 +17,28 @@ const fHandleSubmit = function(oEvent) {
 
     oEvent.preventDefault();
 
+    $form.find(".has-error").removeClass("has-error");
+    $form.find('.alert.alert-danger').remove();
+
     sName = ($nameInput.val() || "").trim();
     sDescription = ($descriptionTextarea.val() || "").trim();
 
     if(!sName) {
         bHasErrors = true;
+        $nameInput.parent().addClass("has-error");
     }
+
+    if(!sDescription) {
+        bHasErrors = true;
+        $descriptionTextarea.parent().addClass("has-error");
+    }
+
+    if(bHasErrors) {
+        $form.find("fieldset").prepend($tplErrorMessage.clone());
+        return console.error("Form has errors.");
+    }
+
+    console.info("All is ok.");
 
     // 2. check them
     // 3. display errors if any
@@ -32,4 +49,5 @@ const fHandleSubmit = function(oEvent) {
 
 $(function() {
     ($form = $("form")).on("submit", fHandleSubmit);
+    $tplErrorMessage = $($("#form-error-message").html());
 });
