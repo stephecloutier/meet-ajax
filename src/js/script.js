c029979735d60717759b40324ba6517fa44f2bf2
stephecloutier <stephecloutier@gmail.com>
2017-03-06 14:04:25 +0100

    Gathered input value

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,24 @@
  * started at 20/02/2017
  */
 
-$( function() {
-    // TODO
-} );
+let $form;
+
+const fHandleSubmit = function(oEvent) {
+    let sName, sDescription,
+        bHasErrors = false;
+
+    oEvent.preventDefault();
+
+    sName = ($form.find("input[name='name']").val() || "").trim();
+    sDescription = ($form.find("textarea[name='description']").val() || "").trim();
+
+    // 2. check them
+    // 3. display errors if any
+    // 4. prepare AJAX request
+    // 5. perform AJAX request
+    // 6. display new buddy
+};
+
+$(function() {
+    ($form = $("form")).on("submit", fHandleSubmit);
+});
